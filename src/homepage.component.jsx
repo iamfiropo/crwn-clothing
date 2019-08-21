cd3890759faffada1e91ab339cfa3c13ca4bb087
Ropo John Olatujoye <princeolatjohnjoe@yahoo.com>
2019-08-21 11:48:06 -0400

    correct jsx error

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,50 +1,43 @@
 import React from 'react';
 
-const HomePage = () => {
-    return (
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
-    )
-}
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>JACKETS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>SHOES</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>WOMENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>MENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default HomePage;
