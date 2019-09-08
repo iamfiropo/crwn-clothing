5eda32e3ad766671471999b6c7170f9a13ef501c
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-09-08 18:02:54 +0100

    add redux persist library for app state persistence add redux persist configuration persist the cart reducer with localStorage storage engine

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -9,6 +10,13 @@ if(process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default () => ({ 
+  /**
+   * Technically, i don't need this function
+   * It can be commented out
+   */
+  store, persistor 
+});
