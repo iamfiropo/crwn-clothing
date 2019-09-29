786c501d87f9b995a96cf433e4af0308218b72e5
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-09-29 14:31:01 +0100

    fetch collection from firestore add with-spinner update the shop selector update shop reducer update shop component update collection page component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,6 +36,34 @@ const config = {
     return userRef;
   }
 
+  export const addCollectionAndDocuments = async (collectionKey, DocumentToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    const batch = firestore.batch();
+    DocumentToAdd.forEach(doc => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, doc);
+    })
+    return await batch.commit();
+  }
+
+  export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {})
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
